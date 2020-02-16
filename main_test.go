84e8c1d370bff9f4f6948e70d2f2f75d3e3091bc
Todd Campbell <todd@toddcampbell.net>
2020-02-16 08:57:38 -0800

    improved test coverage
    Signed-off-by: Todd Campbell <todd@toddcampbell.net>

--- main_test.go
+++ main_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
@@ -15,31 +16,67 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestExecuteHandler(t *testing.T) {
+func TestCheckArgs(t *testing.T) {
 	assert := assert.New(t)
 	event := corev2.FixtureEvent("entity1", "check1")
-	event.Check.Output = "OK"
-
-	var test = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		body, err := ioutil.ReadAll(r.Body)
-		assert.NoError(err)
-		expectedBody := "message=OK&priority=0&title=entity1%2Fcheck1&token=123&user=abc"
-		assert.Equal(expectedBody, strings.Trim(string(body), "\n"))
-		w.WriteHeader(http.StatusOK)
-	}))
-
-	_, err := url.ParseRequestURI(test.URL)
-	require.NoError(t, err)
-	config.PushoverAPIURL = test.URL
+	assert.Error(CheckArgs(event))
+	config.PushoverToken = "abc123"
+	assert.Error(CheckArgs(event))
+	config.PushoverUserKey = "abc123"
+	assert.Error(CheckArgs(event))
 	config.MessageTitleTemplate = "{{.Entity.Name}}/{{.Check.Name}}"
+	assert.Error(CheckArgs(event))
 	config.MessageBodyTemplate = "{{.Check.Output}}"
-	config.OkPriority = 0
-	config.PushoverToken = "123"
-	config.PushoverUserKey = "abc"
-	assert.NoError(sendPushover(event))
+	assert.NoError(CheckArgs(event))
 }
 
-func TestMain(t *testing.T) {
+// Fix this to run repeatable with Status of:
+// 1(yellow,warning)
+// 2(red,critical)
+// 127 (orange,unknown)
+func TestSendPushover(t *testing.T) {
+	testcases := []struct {
+		status   uint32
+		state    string
+		priority uint64
+	}{
+		{0, "OK", 0},
+		{1, "WARNING", 0},
+		{2, "CRITICAL", 1},
+		{127, "UNKNOWN", 1},
+	}
+
+	for _, tc := range testcases {
+		assert := assert.New(t)
+		event := corev2.FixtureEvent("entity1", "check1")
+		event.Check.Status = tc.status
+		event.Check.Output = tc.state
+
+		var test = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			body, err := ioutil.ReadAll(r.Body)
+			assert.NoError(err)
+			// expectedBody := "message=OK&priority=0&title=entity1%2Fcheck1&token=123&user=abc"
+			expectedBody := fmt.Sprintf("message=%s&priority=%v&title=entity1%%2Fcheck1&token=123&user=abc", tc.state, tc.priority)
+			assert.Equal(expectedBody, strings.Trim(string(body), "\n"))
+			w.WriteHeader(http.StatusOK)
+		}))
+
+		_, err := url.ParseRequestURI(test.URL)
+		require.NoError(t, err)
+		config.PushoverAPIURL = test.URL
+		config.MessageTitleTemplate = "{{.Entity.Name}}/{{.Check.Name}}"
+		config.MessageBodyTemplate = "{{.Check.Output}}"
+		config.OkPriority = 0
+		config.WarningPriority = 0
+		config.CriticalPriority = 1
+		config.UnknownPriority = 1
+		config.PushoverToken = "123"
+		config.PushoverUserKey = "abc"
+		assert.NoError(SendPushover(event))
+	}
+}
+
+func Testmain(t *testing.T) {
 	assert := assert.New(t)
 	file, _ := ioutil.TempFile(os.TempDir(), "sensu-go-pushover-handler")
 	defer func() {
